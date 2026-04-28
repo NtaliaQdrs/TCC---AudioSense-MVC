f6f9160345f1461a899bc251da7a36c3d3fd1f40
Évelyn Beatriz <evelynbeatriz1011@gmail.com>
2026-04-28 18:52:33 -0300

    formulário aduiodescrição

--- routes/index.js
+++ routes/index.js
@@ -14,5 +14,14 @@ router.get('/deficiencia-visual', function (req, res, next) {
   res.render('deficienciaVisual', { title: 'Deficiência Visual' });
 });
 
+router.get('/inserir-audiodescricao', function (req, res, next) {
+  res.render('inserirAudiodescricao', { title: 'Inserir Audiodescrição' });
+});
+
+router.post('/inserir-audiodescricao', function (req, res, next) {
+  // Após o clique no botão de submit, o servidor redireciona o usuário
+  res.redirect('/audiodescricao');
+});
+
 
 module.exports = router;
